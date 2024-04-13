daf80014ae097cb0e461297ecde6c7f14455c098
Taisia <tmertz@usc.edu>
2024-04-12 17:55:34 -0700

    added pages

---
+++ src/components/IrrigationPage.js
@@ -0,0 +1,10 @@
+function IrrigationPage() {
+    return (
+      <div className="flex flex-col p-2 w-full h-screen bg-[#eff0ec]">
+        a
+      </div>
+    );
+  }
+  
+  export default IrrigationPage;
+  
---
+++ src/components/LightingPage.js
@@ -0,0 +1,9 @@
+function LightingPage() {
+  return (
+    <div className="flex flex-col p-2 w-full h-screen bg-[#eff0ec]">
+      a
+    </div>
+  );
+}
+
+export default LightingPage;
